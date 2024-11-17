f31e4f23631bec3da6fc92ea6c59ed33aee51911
huzaifa56jbh <hadrish153@gmail.com>
2024-11-17 18:49:09 +0500

    badhsha

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 const app = express()
 
-const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+// const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 
-app.use(cors({
-    origin: allowedOrigin,
-    credentials: true
-}));
-
+app.use(cors()); 
 app.use(express.json({limit:"16kb"}))
 
 app.use(express.urlencoded({
