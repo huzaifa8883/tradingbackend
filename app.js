86f53cb63e0c3f01ff4b3d78b7582d3528b833d3
huzaifa56jbh <hadrish153@gmail.com>
2024-11-17 19:12:06 +0500

    aklwa

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = express()
 
 // const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 
-app.use(cors()); 
+const corsOptions = {
+    origin: 'https://7stardigitizing.com',  // Specify your frontend URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
+  
 app.use(express.json({limit:"16kb"}))
 
 app.use(express.urlencoded({
