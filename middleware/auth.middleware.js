792e24dd45f0a8d0b95ee92cbc21866442349fee
huzaifa56jbh <hadrish153@gmail.com>
2024-11-17 19:46:05 +0500

    baja

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,15 +5,15 @@ import { User } from "../models/user.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-         const token = req.cookies?.accessToken|| req.header("Authorization")?.replace("Bearer ", "")
+         const token = req.cookies?.accessToken
         //  || req.header("Authorization")?.replace("Bearer ", "")
         
-console.log("Extracted Token:",token);
+// console.log("Extracted Token:",token);
 
-         console.log(token);
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
+//          console.log(token);
+//         if (!token) {
+//             throw new ApiError(401, "Unauthorized request")
+//         }
     
         const decodedToken =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
